b10e0c6243f9d362b3ad1cd0ff6a0dfa23521ba9
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-10-15 21:40:41 -0700

    Include Node in directory; set up Stack and StackTest classes.

---
+++ array_stacks/Node.java
@@ -0,0 +1,22 @@
+public class Node {
+  private int value;
+  private Node nextNode;
+
+  public Node(int value) {
+    this.value = value;
+    nextNode = null;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  public Node getNextNode() {
+    return nextNode;
+  }
+
+  public void setNextNode(Node nextNode) {
+    this.nextNode = nextNode;
+  }
+
+}
---
+++ array_stacks/StackTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+public class StackTest {
+  private Stack stack;
+
+  @Before
+  public void setup() {
+    stack = new Stack();
+  }
+
+}
